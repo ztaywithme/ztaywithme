4cc657a706331d446bb333597c4a06cdf398c285
ztaywithme <nguyenloi.me@gmail.com>
2019-05-22 20:43:58 +0700

    aa

---
+++ day2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Câu lệnh điều kiện if else</h1>
+	<?php 
+		$age = 25;
+		if ($age > 18) {
+			echo "Người trưởng thành";
+		} else {
+			echo "<br> Trẻ em";
+		}
+	?>
+</body>
+</html>
